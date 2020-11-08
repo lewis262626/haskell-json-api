2ff6894b3525c6408d7afcbd5ed273ed3e8ca882
EC2 Default User <ec2-user@ip-172-31-16-207.eu-west-1.compute.internal>
2020-11-08 22:01:04 +0000

    hello world api

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Web.Scotty
+import Data.Monoid ((<>))
+import Data.Text.Lazy as T
+
+printCase :: T -> T
+printCase text = T.toUpper text
 
 main :: IO ()
 main = do
   putStrLn "Starting web server..."
+  scotty 3000 $ do
+    get "/hello/:name" $ do
+      name <- param "name"
+      text  $ "Hello " <>  printCase name <> "!"
